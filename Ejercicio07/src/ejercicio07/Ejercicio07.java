ad6f6f4429955b5123f52da46a8a73cc8670325f
Pablo Costa <pacosta2@utpl.edu.ec>
2021-10-29 17:16:45 -0500

    segundo commit

--- Ejercicio07/src/ejercicio07/Ejercicio07.java
+++ Ejercicio07/src/ejercicio07/Ejercicio07.java
@@ -7,7 +7,7 @@ package ejercicio07;
 
 /**
  *
- * @author SALAA
+ * @author PabloCosta14
  */
 public class Ejercicio07 {
 
@@ -15,6 +15,22 @@ public class Ejercicio07 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int variable1 = 81;
+        int variable2 = 9;
+        int variable3 = 3;
+        int variable4 = 9;
+        int variable5 = 10;
+        int variable6 = 1;
+        int variable7 = 100;
+        int variable8 = 25;
+        int variable9 = 100;
+        
+        boolean resultado = (Math.sqrt(variable1)) + variable2 / variable3 == 
+                variable4 || (variable5 > variable6) && (variable7 / variable8) > 
+                Math.sqrt(variable9); 
+        
+        System.out.println(resultado);
+                
         // TODO code application logic here
     }
     
